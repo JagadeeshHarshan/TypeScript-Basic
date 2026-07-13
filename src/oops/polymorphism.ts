f6f24ceb2aa2a7e6ddcdbf7f274681292fa6aba8
JagadeeshHarshan <jagadeshharshan2@gmail.com>
2026-07-13 16:10:41 +0530

    change

---
+++ src/oops/polymorphism.ts
@@ -0,0 +1,61 @@
+// class employee { 
+//     work () {
+
+//     console.log("employee is working");
+//     } 
+// }
+// class Developer extends employee {
+//     work() {
+//         console.log("Developer is writing code");
+//     }
+// }
+// class Tester extends employee {
+//     work() {
+//         console.log("tester is testing application");
+//     }
+// }
+// class Manager extends employee {
+//     work() {
+//         console.log("manager is conducting team meeting");
+//     }
+// }
+// const developer = new Developer();
+// const tester = new Tester();
+// const manager = new Manager ();
+
+// developer.work();
+// tester.work();
+// manager.work();
+
+
+// --------------------------------------------------------------------------------
+
+//class BankAccount {
+//     calculateInterest () {
+//         console.log("calculating interest");
+//     }
+// }
+// class SavingsAccount extends BankAccount{
+//     calculateInterest () {
+//         console.log("savings account interest : 5%");
+//     }
+// }
+// class CurrentAccount extends BankAccount{
+//     calculateInterest () {
+//         console.log("current account interest: 2%");
+//     }
+// }
+// class FixedDeposit extends BankAccount{
+//     calculateInterest() {
+//         console.log("Fixed Deposit interest: 8%");
+//     }
+// }
+
+// const Savings = new SavingsAccount();
+// const Current = new CurrentAccount();
+// const FixedD = new FixedDeposit();
+
+// Savings.calculateInterest();
+// Current.calculateInterest();
+// FixedD.calculateInterest();
+
