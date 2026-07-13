48fd53fc24f6f633fa1885ac4b09b4134501204f
JagadeeshHarshan <jagadeshharshan2@gmail.com>
2026-07-13 16:07:37 +0530

    code

---
+++ src/oops/baseclass.ts
@@ -0,0 +1,25 @@
+// class Car {
+//     brand: string;
+//     model: string;
+
+//     constructor(brand: string, model: string) {
+//         this.brand = brand;
+//         this.model = model;
+//     }
+
+//     start (){
+//         console.log(this.brand + " is starting");
+//     }
+
+//     displayDetails(): void {
+//         console.log("Car Brand:", this.brand);
+//         console.log("Car Model:", this.model);
+//     }
+// }
+
+// const car1 = new Car("TATA", "HARRIER");
+
+// car1.displayDetails();
+// car1.start();
+
+
